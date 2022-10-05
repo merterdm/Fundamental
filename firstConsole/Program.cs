742e1fdc0fc9626f75d30d5c28bf4e749917e793
ISKUR <ISKUR@DESKTOP-MO5HLC4>
2022-10-05 19:36:27 +0300

    Add project files.

---
+++ firstConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace firstConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte sayi = 0;
+            Console.WriteLine(sayi);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
